aae6f796f637a08935ccf22d25042439080c079d
Tobias Fellinger <tobias.fellinger@ages.at>
2022-11-15 17:09:32 +0100

    updated test script

--- scripts/test_script.R
+++ scripts/test_script.R
@@ -1,3 +1,19 @@
+# How To use this test-script
+#
+# The installation of the packages can prompt for input in the console if some
+# packages are not yet installed.
+# It's best to run the lines with install.packages and remotes::install_git
+# interactively one time and then sourcing the whole file.
+#
+# Please don't run the rest of the file line by line, but source the whole
+# script in one go since the script also measures running time of the
+# simulations.
+#
+# The number of simulations is set in the variable N_sim and is currently 250.
+# This is very low and should therefore run quickly. If the script runs without
+# error for 250 simulations, please increase N_sim to 2500 and rund the script
+# again.
+
 # don't stop with an error if a package masks another function
 options(conflicts.policy = NULL)
 
@@ -27,7 +43,7 @@ times <- numeric(0)
 
 times <- c(times, "start"=Sys.time())
 
-N_sim <- 2500
+N_sim <- 250
 Assumptions <- assumptions_delayed_effect()
 Options <- design_fixed_followup()
 Design <- merge(Assumptions, Options, by=NULL)
@@ -52,7 +68,7 @@ Summarise <- create_summarise_function(
 
 times <- c(times, "test_1_setup"=Sys.time())
 
-res <- runSimulation(
+result_1 <- runSimulation(
   Design,
   replications = N_sim,
   generate = my_generator,
@@ -100,7 +116,7 @@ Summarise <- create_summarise_function(
 
 times <- c(times, "test_2_setup"=Sys.time())
 
-res <- runSimulation(
+result_2 <- runSimulation(
   Design,
   replications = N_sim,
   generate = my_generator,
@@ -130,7 +146,7 @@ Analyse <- list(
 
 times <- c(times, "test_3_setup"=Sys.time())
 
-res <- runSimulation(
+result_3 <- runSimulation(
   Design,
   replications = N_sim,
   generate = my_generator,
@@ -148,8 +164,33 @@ times <- c(times, "test_3_sim"=Sys.time())
 performance <- list(
   n_cores = n_cores,
   sessioninfo = sessionInfo(),
-  duration = diff(times)
+  duration = diff(times),
+  N_sim = N_sim
 )
 
 filename <- paste0(Sys.info()["nodename"], "_", strftime(Sys.time(), "%Y-%m-%d_%H%M%S"), ".Rds")
 saveRDS(performance, filename)
+
+
+# output of the simulations -----------------------------------------------
+# this script is mainly to check if the simulations run on each computer
+# but some simulations are done (yet with unrealistic parameters)
+# if you want to inspect the results, use the commented code below.
+
+# result_1 |>
+#   subset(select=c("delay", "maxcombo.rejection", "logrank.rejection"))
+#
+# result_2 |>
+#   subset(select=c(
+#     "delay",
+#     "maxcombo_seq.rejection", "logrank_seq.rejection",
+#     "maxcombo_seq.n_pat", "logrank_seq.n_pat",
+#     "maxcombo_seq.n_evt", "logrank_seq.n_evt",
+#     "maxcombo_seq.followup", "logrank_seq.followup"
+#   ))
+#
+# result_3 |>
+#   subset(select=c(
+#     "delay", "coxph.HR.bias", "coxph.gAHR.bias", "coxph.HR.mse",
+#     "coxph.gAHR.mse", "coxph.HR.coverage", "coxph.gAHR.coverage"
+#   ))
