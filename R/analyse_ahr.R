ba3cd0bce14fb7a82e9882b5386c018d2696dba2
Tobias Fellinger <tobias.fellinger@ages.at>
2022-11-17 14:33:27 +0100

    analyse AHR

---
+++ R/analyse_ahr.R
@@ -0,0 +1,76 @@
+#' Analyse the dataset using extimators for the the average hazard ratio
+#'
+#' @param max_time time for which the RMST is calculated
+#' @param type=c("AHR","gAHR") AHR for average hazard ratio gAHR for geometric average hazard ratio
+#'
+#' @return Returns an analysis function, that can be used in runSimulations
+#'
+#' @export
+#'
+#' @details
+#' The implementation from the nph package is used, see the documentation there
+#' for details.
+#'
+#' The data.frame returned by the created function includes the follwing
+#' columns:
+#'
+#' * `p` p value of the test, see Details
+#' * `AHR`/`gAHR` estimated differnce in RMST
+#' * `AHR_lower`/`gAHR_lower` adjusted lower bound of the confidence interval for differnce in RMST
+#' * `AHR_upper`/`gAHR_upper` adjusted upper bound of the confidence interval for differnce in RMST
+#' * `N_pat` number of patients
+#' * `N_evt` number of events
+#'
+#' @seealso
+#' [nph:nphparams]
+#'
+#' @examples
+#' condition <- merge(
+#'     assumptions_delayed_effect(),
+#'     design_fixed_followup(),
+#'     by=NULL
+#'   ) |>
+#'   head(1)
+#' dat <- generate_delayed_effect(condition)
+#' analyse_ahr()(condition, dat)
+#' analyse_ahr(type="gAHR")(condition, dat)
+#' analyse_ahr(max_time=50, type="AHR")(condition, dat)
+#' analyse_ahr(max_time=50, type="gAHR")(condition, dat)
+analyse_ahr <- function(max_time=NA, type="AHR"){
+
+  switch(
+    type,
+    AHR = {
+      function(condition, dat, fixed_objects = NULL){
+        model <- nph::nphparams(dat$t, dat$evt, dat$trt, param_type="avgHR", param_par=max_time)
+
+        list(
+          p = model$tab$p_adjusted,
+          AHR = model$tab$Estimate,
+          AHR_lower = model$tab$lwr_adjusted,
+          AHR_upper = model$tab$upr_adjusted,
+          N_pat=nrow(dat),
+          N_evt=sum(dat$evt)
+        )
+      }
+    },
+    gAHR = {
+      function(condition, dat, fixed_objects = NULL){
+        model <- nph::nphparams(dat$t, dat$evt, dat$trt, param_type="HR", param_par=max_time)
+
+        list(
+          p = model$tab$p_adjusted,
+          gAHR = model$tab$Estimate,
+          gAHR_lower = model$tab$lwr_adjusted,
+          gAHR_upper = model$tab$upr_adjusted,
+          N_pat=nrow(dat),
+          N_evt=sum(dat$evt)
+        )
+      }
+    },
+    {
+      stop(gettext("Invalid type, use AHR for the average hazard ratio or gAHR for the geometric average hazard ratio."))
+    }
+  )
+
+}
---
+++ tests/testthat/test-analyse_ahr.R
@@ -0,0 +1,37 @@
+test_that("analyse ahr works", {
+  capture.output({
+    condition <- merge(
+      assumptions_delayed_effect(),
+      design_fixed_followup(),
+      by=NULL
+    ) |>
+      head(1)
+  })
+
+  dat <- generate_delayed_effect(condition)
+
+  expect_type(analyse_ahr(), "closure")
+  expect_error(analyse_ahr(type="something else"))
+
+  results1 <- analyse_ahr()(condition, dat)
+  results2 <- analyse_ahr(type="gAHR")(condition, dat)
+  results3 <- analyse_ahr(max_time=50, type="AHR") (condition, dat)
+  results4 <- analyse_ahr(max_time=50, type="gAHR")(condition, dat)
+
+  expect_type(results1, "list")
+  expect_s3_class(results1, NA)
+  expect_named(results1, c("p", "AHR", "AHR_lower", "AHR_upper", "N_pat", "N_evt"))
+
+  expect_type(results2, "list")
+  expect_s3_class(results2, NA)
+  expect_named(results2, c("p", "gAHR", "gAHR_lower", "gAHR_upper", "N_pat", "N_evt"))
+
+  expect_type(results3, "list")
+  expect_s3_class(results3, NA)
+  expect_named(results3, c("p", "AHR", "AHR_lower", "AHR_upper", "N_pat", "N_evt"))
+
+  expect_type(results4, "list")
+  expect_s3_class(results4, NA)
+  expect_named(results4, c("p", "gAHR", "gAHR_lower", "gAHR_upper", "N_pat", "N_evt"))
+
+})
