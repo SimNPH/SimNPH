f3536139f5ec2647b81adce1c76d1f7a4cee4d1a
Tobias Fellinger <tobias.fellinger@ages.at>
2022-11-22 15:32:39 +0100

    added empty functions for analysis functions

---
+++ tests/testthat/test-analyse_piecewise_exponential.R
@@ -0,0 +1,16 @@
+test_that("analyse_piecewise_exponential outputs plausible data.frame for delayed effect", {
+  capture_output(
+    condition <- merge(
+      assumptions_delayed_effect(),
+      design_fixed_followup(),
+      by=NULL
+    ) |>
+      head(1)
+  )
+  dat <- generate_delayed_effect(condition)
+  res <- analyse_piecewise_exponential(time=c(30, 360))(condition, dat)
+
+  expect_true(hasName(res, "p"), label="result has the column p")
+  expect_lte(res$p, 1, label="p value le 1")
+  expect_gte(res$p, 0, label="p value ge 0")
+})
