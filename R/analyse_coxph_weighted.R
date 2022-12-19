602a769d1457cfa8b82ff4622d76e5030c3132cd
Tobias Fellinger <tobias.fellinger@ages.at>
2022-12-19 14:28:17 +0100

    added different weights to coxph weighted

--- R/analyse_coxph_weighted.R
+++ R/analyse_coxph_weighted.R
@@ -15,10 +15,10 @@
 #'
 #' @details
 #'
-#' Type can be either "SG" or "S". For "SG" the weights are set to
-#' `S(t)*G(t)^(-1)`, for "S" `S(t)` is used. Here `S(t)` denotes the Kaplan
-#' Meier estimator for the event Times and `G(t)` denotes the Kaplan Meier
-#' estimator for the censoring times.
+#' Type can be "SG", "S" or "G". For "SG" the weights are set to
+#' `S(t)*G(t)^(-1)`, for "S" `S(t)` is used, for "G", `G(t)^(-1)` is used. Here
+#' `S(t)` denotes the Kaplan Meier estimator for the event Times and `G(t)`
+#' denotes the Kaplan Meier estimator for the censoring times.
 #'
 #' @examples
 #' condition <- merge(
@@ -30,19 +30,33 @@
 #' dat <- generate_delayed_effect(condition)
 #' analyse_coxph_weighted()(condition, dat)
 analyse_coxph_weighted <- function(type="SG"){
-  if(!(type %in% c("SG", "S"))){
+  if(!(type %in% c("SG", "S", "G"))){
     stop('in analyse_coxph_weighted: invalid "type", currently implemented: "SG" and "G".')
   }
 
   function(condition, dat, fixed_objects = NULL){
 
-    S <- survfit(Surv(t, evt)~1, data=dat)
-    weights <- S$surv[match(dat$t, S$time)]
+    # set t_max to last non-censored observation to avoid infinite weights
+    t_max <- max(dat$t[dat$evt])
+    dat <- dat[dat$t <= t_max, ]
 
-    if(type=="SG"){
-      W <- survfit(Surv(t, !evt)~1, data=dat)
-      weights <- weights * ((W$surv[match(dat$t, W$time)])^(-1))
-    }
+    switch(
+      type,
+      S = {
+        S <- survfit(Surv(t, evt)~1, data=dat)
+        weights <- S$surv[match(dat$t, S$time)]
+      },
+      SG = {
+        S <- survfit(Surv(t, evt)~1, data=dat)
+        weights <- S$surv[match(dat$t, S$time)]
+        W <- survfit(Surv(t, !evt)~1, data=dat)
+        weights <- weights * ((W$surv[match(dat$t, W$time)])^(-1))
+      },
+      G={
+        W <- survfit(Surv(t, !evt)~1, data=dat)
+        weights <- ((W$surv[match(dat$t, W$time)])^(-1))
+      }
+    )
 
     model <- survival::coxph(survival::Surv(t, evt) ~ trt, dat, robust=TRUE, weights=weights, subset=(weights!=0))
     model_summary <- summary(model)
@@ -54,7 +68,8 @@ analyse_coxph_weighted <- function(type="SG"){
       hr_lower = model_summary$conf.int[, "lower .95"],
       hr_upper = model_summary$conf.int[, "upper .95"],
       N_pat=nrow(dat),
-      N_evt=sum(dat$evt)
+      N_evt=sum(dat$evt),
+      followup=t_max
     )
   }
 }
