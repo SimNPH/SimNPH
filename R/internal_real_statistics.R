1f4e238664eb7aad21e2c5722bf601f784886d43
Tobias Fellinger <tobias.fellinger@ages.at>
2023-01-04 16:09:08 +0100

    added version of real statistics that use discrete_approximation from nph

--- R/internal_real_statistics.R
+++ R/internal_real_statistics.R
@@ -1,39 +1,69 @@
-internal_real_statistics_pchaz <- function(data_gen_model_trt, data_gen_model_ctrl, N_trt=1, N_ctrl=1, cutoff=1000){
+internal_real_statistics_pchaz_discrete <- function(data_gen_model_trt, data_gen_model_ctrl, N_trt=1, N_ctrl=1, cutoff=1000, milestones=NULL){
+  stopifnot(
+    length(data_gen_model_trt$t) == length(data_gen_model_ctrl$t)
+  )
 
-  real_stats_one_group <- function(group, cutoff, milestones){
-    res <- data.frame(
-      rmst      = integrate(group$funs$surv_f, 0, cutoff)$value,
-      median_survival = if(group$funs$surv_f(max(group$t, cutoff)) < 0.5) {
-        uniroot(\(x) group$funs$surv_f(x) - 0.5, lower=0, upper=12, extendInt = "downX")$root
-      } else {
-        Inf
-      }
+  stopifnot(
+    all(data_gen_model_trt$t == data_gen_model_ctrl$t)
+  )
+
+  h <- data_gen_model_trt$haz + data_gen_model_ctrl$haz
+  f1 <- c(diff(data_gen_model_trt$F), 0)
+  f2 <- c(diff(data_gen_model_ctrl$F), 0)
+  f <- (1/(N_trt+N_ctrl))*(N_trt*f1 + N_ctrl*f2)
+
+  res <- data.frame(
+    rmst_trt            = sum(data_gen_model_trt$S[data_gen_model_trt$t <= cutoff]),
+    median_survival_trt = ifelse(
+      any(data_gen_model_trt$F >= 0.5),
+      data_gen_model_trt$t[data_gen_model_trt$F >= 0.5][1],
+      Inf
+    ),
+    rmst_ctrl           = sum(data_gen_model_ctrl$S[data_gen_model_ctrl$t <= cutoff]),
+    median_survival_ctrl= ifelse(
+      any(data_gen_model_ctrl$F >= 0.5),
+      data_gen_model_ctrl$t[data_gen_model_ctrl$F >= 0.5][1],
+      Inf
+    ),
+    gAHR                = exp(sum( (log(data_gen_model_trt$haz / data_gen_model_ctrl$haz) * f)[data_gen_model_trt$t <= cutoff] , na.rm=TRUE)),
+    AHR                 = sum(((data_gen_model_trt$haz/h) * f)[data_gen_model_trt$t <= cutoff], na.rm=TRUE) /
+      sum(((data_gen_model_ctrl$haz/h) * f)[data_gen_model_ctrl$t <= cutoff], na.rm=TRUE)
+  )
+
+  if(!is.null(milestones)){
+    if(is.null(names(milestones))){
+      my_colnames <- paste0("milestone_surv_", milestones)
+    } else {
+      my_colnames <- names(milestones)
+    }
+
+    milestones_trt <- milestones |>
+      sapply(\(v){
+        tail(data_gen_model_trt$S[data_gen_model_trt$t < v], 1)
+      }) |>
+      setNames(paste0(my_colnames, "_trt")) |>
+      t() |>
+      as.data.frame()
+
+    milestones_ctrl <- milestones |>
+      sapply(\(v){
+        tail(data_gen_model_ctrl$S[data_gen_model_ctrl$t < v], 1)
+      }) |>
+      setNames(paste0(my_colnames, "_ctrl")) |>
+      t() |>
+      as.data.frame()
+
+    res <- cbind(
+      res,
+      milestones_trt,
+      milestones_ctrl
     )
 
-    res
   }
 
-  real_stats_trt  <- real_stats_one_group(data_gen_model_trt, cutoff = cutoff, milestones = milestones)
-  real_stats_ctrl <- real_stats_one_group(data_gen_model_ctrl, cutoff = cutoff, milestones = milestones)
-
-  # helper functions for average hazard ratios
-  # notation corresponds to Schemper 2009
-  h1 <- data_gen_model_trt$funs$haz_f
-  h2 <- data_gen_model_ctrl$funs$haz_f
-  h  <- \(t){h1(t)+h2(t)}
-  f  <- \(t){(1/(N_trt+N_ctrl))*(N_trt*data_gen_model_trt$funs$pdf_f(t) + N_ctrl*data_gen_model_ctrl$funs$pdf_f(t))}
-  w  <- \(t){1} # Cox
-  # w  <- \(t){(1/(N_trt+N_ctrl))*(N_trt*group_1$funs$surv_f(t) + N_ctrl*group_2$funs$surv_f(t))} # WCox
-
-  gAHR <- exp(integrate(\(t){log(h1(t) / h2(t)) * f(t) * w(t)}, 0, cutoff)$value)
-  AHR  <- integrate(\(t){(h1(t)/h(t)) * f(t) * w(t)}, 0, cutoff)$value / integrate(\(t){(h2(t)/h(t)) * f(t) * w(t)}, 0, cutoff)$value
-
-
-  names(real_stats_trt) <- paste0(names(real_stats_trt), "_trt")
-  names(real_stats_ctrl) <- paste0(names(real_stats_ctrl), "_ctrl")
-
-  cbind(real_stats_trt, real_stats_ctrl, gAHR=gAHR, AHR=AHR)
+  res
 }
+
 
 
 fast_real_statistics <- function(
--- tests/testthat/test-internal_real_statistics.R
+++ tests/testthat/test-internal_real_statistics.R
@@ -29,18 +29,39 @@ test_that("internal function for real summary statistics outputs correct values"
     c(condition$hazard_ctrl/1000)
   )
 
-  result_1 <- internal_real_statistics_pchaz(treatment, control)
-  result_2 <- internal_real_statistics_pchaz(treatment, treatment)
-  result_3 <- internal_real_statistics_pchaz(control, control)
-  result_4 <- internal_real_statistics_pchaz(control, control_low_rate)
+  control_different_t <- nph::pchaz(
+    c(0, 100),
+    c(condition$hazard_ctrl)
+  )
+
+  control_different_t_2 <- control
+  control_different_t_2$t[50] <- -1
+
+  treatment_10 <- nph::pchaz(
+    c(0, condition$delay, t_max)*10,
+    c(condition$hazard_ctrl, condition$hazard_trt)/10
+  )
+
+  control_10 <- nph::pchaz(
+    c(0, t_max)*10,
+    c(condition$hazard_ctrl)/10
+  )
+
+  result_1 <- internal_real_statistics_pchaz_discrete(treatment, control, milestones=c(msurv_5=5, msurv_10=10, msurv_15=15))
+  result_2 <- internal_real_statistics_pchaz_discrete(treatment, treatment, milestones=c(5, 10, 15))
+  result_3 <- internal_real_statistics_pchaz_discrete(control, control)
+  result_4 <- internal_real_statistics_pchaz_discrete(control, control_low_rate)
 
   result_1_b <- fast_real_statistics_pchaz(treatment$Tint[-3], treatment$lambda, control$Tint[-2], control$lambda, milestones=c(msurv_5=5, msurv_10=10, msurv_15=15))
   result_2_b <- fast_real_statistics_pchaz(treatment$Tint[-3], treatment$lambda, treatment$Tint[-3], treatment$lambda, milestones=c(5, 10, 15))
   result_3_b <- fast_real_statistics_pchaz(control$Tint[-2], control$lambda, control$Tint[-2], control$lambda)
 
+  result_1_c <- internal_real_statistics_pchaz_discrete(treatment_10, control_10, milestones=c(msurv_5=5, msurv_10=10, msurv_15=15)*10)
+  result_1_c[1:4] <- result_1_c[1:4] / 10
+
   # correct class, names, etc.
   expect_s3_class(result_1, "data.frame")
-  expect_named(result_1, c("rmst_trt", "median_survival_trt", "rmst_ctrl", "median_survival_ctrl", "gAHR", "AHR"))
+  expect_named(result_3, c("rmst_trt", "median_survival_trt", "rmst_ctrl", "median_survival_ctrl", "gAHR", "AHR"))
 
   expect_s3_class(result_1_b, "data.frame")
   expect_named(result_1_b, c("rmst_trt", "median_survival_trt", "rmst_ctrl", "median_survival_ctrl", "gAHR", "AHR",
@@ -62,6 +83,8 @@ test_that("internal function for real summary statistics outputs correct values"
   expect_equal(result_3_b$gAHR, 1)
   expect_equal(result_3_b$AHR, 1)
 
-  result_4
+  expect_error(internal_real_statistics_pchaz_discrete(treatment, control_different_t))
+  expect_error(internal_real_statistics_pchaz_discrete(treatment, control_different_t_2))
 
+  expect(all(abs((result_1_c / result_1_b) - 1) < 0.15), "relative error between discrete approximation with 1/10 interval and exact calculation is less than 15%")
 })
