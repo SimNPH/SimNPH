26a6296bfbf64d33d645fa042bbdf0d784f7de18
Tobias Fellinger <tobias.fellinger@ages.at>
2023-02-08 11:33:41 +0100

    implemented weibull regression function using Code provided by Andrew

--- R/analyse_weibull.R
+++ R/analyse_weibull.R
@@ -1,12 +1,16 @@
 #' Analyse Dataset with Weibull Regression
 #'
-#' @return an analysis function that returns a data.frame with the columns
-#' * `est_med_trt` estimated median survival in the treatment arm
-#' * `est_med_ctrl` estimated median survival in the control arm
-#' * `N_pat` number of patients
-#' * `N_evt` number of events
+#' @return an analysis function that returns a data.frame
 #'
 #' @export
+#'
+#' @details the columns in the return are the two-sided p-value for the test of
+#'   equal medians. The estimated medians in the treatment and control group and
+#'   the estimated difference in median survival with confidence intervals.
+#'
+#'   The estimates and tests are comstructed by fitting seperate Weibull
+#'   regression models in the treatment and control groups and then estimating
+#'   the medians and respective variances with the delta-method.
 #'
 #' @examples
 #' condition <- merge(
@@ -20,15 +24,70 @@
 #' analyse_weibull()(condition, dat)
 analyse_weibull <- function() {
   function(condition, dat, fixed_objects = NULL){
-    model_trt <- survreg(Surv(t, evt) ~ 1, dat, dist="weibull", subset = (trt==1))
-    model_ctrl <- survreg(Surv(t, evt) ~ 1, dat, dist="weibull", subset = (trt==0))
+    # Calculations provided by Andrew Hooker
 
-    med_trt <- exp(model_trt$coefficients["(Intercept)"]) * (log(2)^(model_trt$scale)) - 1
-    med_ctrl <- exp(model_ctrl$coefficients["(Intercept)"]) * (log(2)^(model_ctrl$scale)) - 1
+    reg_1 <- survreg(Surv(t, evt) ~ 1, dat, dist="weibull", subset = (trt==1))
+    reg_0 <- survreg(Surv(t, evt) ~ 1, dat, dist="weibull", subset = (trt==0))
+
+
+    params_surv_0 <- c(
+      "intercept"=reg_0$coefficients[[1]],
+      "log_scale"=reg_0$scale
+    )
+    cov_0 <- reg_0$var
+    rownames(cov_0) <- names(params_surv_0)
+    colnames(cov_0) <- names(params_surv_0)
+
+    params_surv_1 <- c(
+      "intercept"=reg_1$coefficients[[1]],
+      "log_scale"=reg_1$scale
+    )
+    cov_1 <- reg_1$var
+    rownames(cov_1) <- names(params_surv_1)
+    colnames(cov_1) <- names(params_surv_1)
+
+    med_tte_0 <- car::deltaMethod(
+      params_surv_0,
+      "exp(intercept)*log(2)^(log_scale)",
+      cov_0
+    )
+
+    med_tte_1 <- car::deltaMethod(
+      params_surv_1,
+      "exp(intercept)*log(2)^(log_scale)",
+      cov_1
+    )
+
+    params_surv_both <- c(params_surv_0,params_surv_1)
+    names(params_surv_both) <- c("intercept_0","log_scale_0","intercept_1","log_scale_1")
+
+    cov_both <- diag(4)
+    cov_both[1:2,1:2] <- cov_0
+    cov_both[3:4,3:4] <- cov_1
+    rownames(cov_both) <- names(params_surv_both)
+    colnames(cov_both) <- names(params_surv_both)
+
+    diff_med_tte <- car::deltaMethod(
+      params_surv_both,
+      "exp(intercept_1)*log(2)^(log_scale_1) - exp(intercept_0)*log(2)^(log_scale_0)",
+      cov_both
+    )
+
+    z_val <- (diff_med_tte$Estimate)/diff_med_tte$SE
+    p_val_1sided <- 1-pnorm(z_val)
+    p_val_2sided <- 1-pchisq(z_val^2, 1)
 
     list(
-      est_med_trt = med_trt,
-      est_med_ctrl = med_ctrl,
+      p = p_val_2sided,
+      med_trt_est = med_tte_0[, "Estimate"],
+      med_trt_lower = med_tte_0[, "2.5 %"],
+      med_trt_upper = med_tte_0[, "97.5 %"],
+      med_ctrl_est = med_tte_1[, "Estimate"],
+      med_ctrl_lower = med_tte_1[, "2.5 %"],
+      med_ctrl_upper = med_tte_1[, "97.5 %"],
+      diff_med_est = diff_med_tte[, "Estimate"],
+      diff_med_lower = diff_med_tte[, "2.5 %"],
+      diff_med_upper = diff_med_tte[, "97.5 %"],
       N_pat=nrow(dat),
       N_evt=sum(dat$evt)
     )
