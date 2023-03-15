7533681e83225f5f8ad5c863cadc702e642fd34f
Tobias Fellinger <tobias.fellinger@ages.at>
2023-03-15 15:59:13 +0100

    changes from other branches

--- scripts/run_simulations_common.R
+++ scripts/run_simulations_common.R
@@ -2,27 +2,27 @@
 
 my_analyse <- list(
   # estimation
-  ahr_6m   = analyse_ahr(type="AHR", max_time=m2d(6)),
-  ahr_12m  = analyse_ahr(type="AHR", max_time=m2d(12)),
-  gahr_6m  = analyse_ahr(type="gAHR", max_time=m2d(6)),
-  gahr_12m = analyse_ahr(type="gAHR", max_time=m2d(12)),
-  median_surv = analyse_diff_median_survival(),
-  milestone = analyse_milestone_survival(times=m2d(c(6, 12))),
-  rmst_diff_6m  = analyse_rmst_diff(max_time = m2d(6)),
-  rmst_diff_12m = analyse_rmst_diff(max_time = m2d(12)),
-  cox = analyse_coxph(),
-  weighted_cox_6m  = analyse_coxph_weighted(type="G", max_time=m2d(6)),
-  weighted_cox_12m = analyse_coxph_weighted(type="G", max_time=m2d(12)),
+  ahr_6m   = analyse_ahr(type="AHR", max_time=m2d(6), alternative = "one.sided"),
+  ahr_12m  = analyse_ahr(type="AHR", max_time=m2d(12), alternative = "one.sided"),
+  gahr_6m  = analyse_ahr(type="gAHR", max_time=m2d(6), alternative = "one.sided"),
+  gahr_12m = analyse_ahr(type="gAHR", max_time=m2d(12), alternative = "one.sided"),
+  median_surv = analyse_diff_median_survival(alternative = "one.sided"),
+  milestone = analyse_milestone_survival(times=m2d(c(6, 12)), alternative = "one.sided"),
+  rmst_diff_6m  = analyse_rmst_diff(max_time = m2d(6), alternative = "one.sided"),
+  rmst_diff_12m = analyse_rmst_diff(max_time = m2d(12), alternative = "one.sided"),
+  cox = analyse_coxph(alternative = "one.sided"),
+  weighted_cox_6m  = analyse_coxph_weighted(type="G", max_time=m2d(6), alternative = "one.sided"),
+  weighted_cox_12m = analyse_coxph_weighted(type="G", max_time=m2d(12), alternative = "one.sided"),
   # tests
   pw_exp_3  = analyse_piecewise_exponential(cuts=m2d(seq(0, 240, by= 3)), testing_only=TRUE),
   pw_exp_12 = analyse_piecewise_exponential(cuts=m2d(seq(0, 240, by=12)), testing_only=TRUE),
-  peto_peto = analyse_gehan_wilcoxon(),
-  fh_0_0 = analyse_logrank_fh_weights(rho=0, gamma=0),
-  fh_0_1 = analyse_logrank_fh_weights(rho=0, gamma=1),
-  fh_1_0 = analyse_logrank_fh_weights(rho=1, gamma=0),
-  fh_1_1 = analyse_logrank_fh_weights(rho=1, gamma=1),
-  logrank = analyse_logrank(),
-  max_combo = analyse_maxcombo(),
+  peto_peto = analyse_gehan_wilcoxon(alternative = "one.sided"),
+  fh_0_0 = analyse_logrank_fh_weights(rho=0, gamma=0, alternative = "one.sided"),
+  fh_0_1 = analyse_logrank_fh_weights(rho=0, gamma=1, alternative = "one.sided"),
+  fh_1_0 = analyse_logrank_fh_weights(rho=1, gamma=0, alternative = "one.sided"),
+  fh_1_1 = analyse_logrank_fh_weights(rho=1, gamma=1, alternative = "one.sided"),
+  logrank = analyse_logrank(alternative = "one.sided"),
+  max_combo = analyse_maxcombo(alternative = "one.sided"),
   modest_6 = analyse_modelstly_weighted(t_star=m2d(6)),
   modest_8 = analyse_modelstly_weighted(t_star=m2d(8)),
   # tests group sequential
@@ -30,43 +30,43 @@ my_analyse <- list(
     followup = c(condition$interim_events, condition$final_events),
     followup_type = c("event", "event"),
     alpha = nominal_alpha,
-    analyse_functions = analyse_gehan_wilcoxon()
+    analyse_functions = analyse_gehan_wilcoxon(alternative = "one.sided")
   ),
   fh_gs_0_0 = analyse_group_sequential(
     followup = c(condition$interim_events, condition$final_events),
     followup_type = c("event", "event"),
     alpha = nominal_alpha,
-    analyse_functions = analyse_logrank_fh_weights(rho=0, gamma=0)
+    analyse_functions = analyse_logrank_fh_weights(rho=0, gamma=0, alternative = "one.sided")
   ),
   fh_gs_0_1 = analyse_group_sequential(
     followup = c(condition$interim_events, condition$final_events),
     followup_type = c("event", "event"),
     alpha = nominal_alpha,
-    analyse_functions = analyse_logrank_fh_weights(rho=0, gamma=1)
+    analyse_functions = analyse_logrank_fh_weights(rho=0, gamma=1, alternative = "one.sided")
   ),
   fh_gs_1_0 = analyse_group_sequential(
     followup = c(condition$interim_events, condition$final_events),
     followup_type = c("event", "event"),
     alpha = nominal_alpha,
-    analyse_functions = analyse_logrank_fh_weights(rho=1, gamma=0)
+    analyse_functions = analyse_logrank_fh_weights(rho=1, gamma=0, alternative = "one.sided")
   ),
   fh_gs_1_1 = analyse_group_sequential(
     followup = c(condition$interim_events, condition$final_events),
     followup_type = c("event", "event"),
     alpha = nominal_alpha,
-    analyse_functions = analyse_logrank_fh_weights(rho=1, gamma=1)
+    analyse_functions = analyse_logrank_fh_weights(rho=1, gamma=1, alternative = "one.sided")
   ),
   logrank_gs = analyse_group_sequential(
     followup = c(condition$interim_events, condition$final_events),
     followup_type = c("event", "event"),
     alpha = nominal_alpha,
-    analyse_functions = analyse_logrank()
+    analyse_functions = analyse_logrank(alternative = "one.sided")
   ),
   max_combo_gs = analyse_group_sequential(
     followup = c(condition$interim_events, condition$final_events),
     followup_type = c("event", "event"),
     alpha = nominal_alpha,
-    analyse_functions = analyse_maxcombo()
+    analyse_functions = analyse_maxcombo(alternative = "one.sided")
   ),
   modest_gs_6 = analyse_group_sequential(
     followup = c(condition$interim_events, condition$final_events),
@@ -93,7 +93,7 @@ my_summarise <- create_summarise_function(
   ahr_12m = summarise_estimator(est=AHR, real=AHR_12m),
   gahr_6m  = summarise_estimator(est=gAHR, real=gAHR_6m),
   gahr_12m = summarise_estimator(est=gAHR, real=gAHR_12m),
-  median_surv = summarise_estimator(est=diff_Q, real=median_survival_trt/median_survival_ctrl),
+  median_surv = summarise_estimator(est=diff_Q, real=median_survival_trt-median_survival_ctrl),
   milestone = summarise_estimator(est=milestone_surv_ratio[1], real= milestone_survival_ctrl_6m/ milestone_survival_trt_6m, name="milestone_6" ),
   milestone = summarise_estimator(est=milestone_surv_ratio[2], real=milestone_survival_ctrl_12m/milestone_survival_trt_12m, name="milestone_12"),
   rmst_diff_6m  = summarise_estimator(est=rmst_diff, real=rmst_trt_6m -rmst_ctrl_6m),
