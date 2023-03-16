3848f496c5b5b30d9fff4898adc3fb205031f98a
Tobias Fellinger <tobias.fellinger@ages.at>
2023-03-16 09:49:39 +0100

    added parameter for free scales

--- R/report_plots.R
+++ R/report_plots.R
@@ -101,6 +101,7 @@ order_combine_xvars <- function(data, xvars, facet_vars=c(), height_x_axis=0.8, 
 #' @param heights_plots relative heights of the main plot and the stairs on the bottom
 #' @param scale_stairs height of the stairs for each variable between 0 and 1
 #' @param grid_level depht of loops for which the grid-lines are drawn
+#' @param scales passed on to facet_grid
 #'
 #' @return a ggplot/patchwork object conatining the plots
 #' @export
@@ -120,7 +121,8 @@ combined_plot <- function(
     facet_y_vars=c(),
     heights_plots = c(3,1),
     scale_stairs = 0.75,
-    grid_level = 2
+    grid_level = 2,
+    scales = "fixed"
     ){
 
   if(!all(c("ggplot2", "patchwork") %in% .packages())){
@@ -177,7 +179,8 @@ combined_plot <- function(
     facet_grid(
       cols = vars(!!!facet_vars_x_sym),
       rows = vars(!!!facet_vars_y_sym),
-      labeller = label_both
+      labeller = label_both,
+      scales = scales
     )
 
   (plot_1 / plot_2) + plot_layout(heights=heights_plots)
